78667235258cb476be711dd07d85c8b6884b7e3f
CVNWR72\Administrator <n.siwinsky@gmail.com>
2022-10-26 21:55:44 +0200

    added main page

--- booking_frontend/src/components/MainNavbar.jsx
+++ booking_frontend/src/components/MainNavbar.jsx
@@ -4,21 +4,27 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './MainNavbar.css';
 
-function MainNavbar() {
+const MainNavbar = () => {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    <nav className="navbar navbar-expand-lg navbar-dark bg-white">
+        <div className="container">
+            <a className="navbar-brand text-info" href="/">GetHome.com</a>
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a className="nav-link text-dark" href="#">Contact Us</a>
+                    </li>
+                    <li> 
+                    <button type="button" class="btn btn-light">Log In</button>
+                    <button type="button" class="btn btn-light">Sign Up</button>
+                    </li>
+                </ul>
+            </div>
+
+    </nav>
+)
 }
+
 
 export default MainNavbar;
